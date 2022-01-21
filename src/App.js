528083daf5ec9543608f3714bb83a0a34f60610a
lohith <lohith05amal@gmail.com>
2022-01-20 22:17:29 -0500

    portfolio need fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <>
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <AboutMe />
       <Services />
+      <Portfolio />
     </>
   );
 }
